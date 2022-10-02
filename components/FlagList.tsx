bee2ec67fe99c3666932aa2c106e3ad831c54882
Mark O'Connor <mark.oconnor14@gmail.com>
2022-10-02 23:55:05 +0700

    chore: change span to p in flag list component

--- components/FlagList.tsx
+++ components/FlagList.tsx
@@ -4,8 +4,6 @@ import "../node_modules/flag-icons/css/flag-icons.min.css";
 
 export default function FlagList() {
 
-    
-
     return (
         <article className={`${styles.card}`}>
             <div style={{display: 'flex', justifyContent: 'space-between', alignItems: 'center'}}>
@@ -14,56 +12,56 @@ export default function FlagList() {
             </div>
             <ul>
                 <li className={`${styles.flex} ${styles.flex_between}`}>
-                    <span>L8 W5 D2</span>
+                    <p>L8 W5 D2</p>
                     <div className={`${styles.flex} ${styles.flex_gap}`}>
                         <Link href="#"><a>Play</a></Link>
                         <Link href="#"><a>Edit</a></Link>
                     </div>
                 </li>
                 <li className={`${styles.flex} ${styles.flex_between}`}>
-                    <span>L8 W5 D1</span>
+                    <p>L8 W5 D1</p>
                     <div className={`${styles.flex} ${styles.flex_gap}`}>
                         <Link href="#"><a>Play</a></Link>
                         <Link href="#"><a>Edit</a></Link>
                     </div>
                 </li>
                 <li className={`${styles.flex} ${styles.flex_between}`}>
-                    <span>L5 W5 D2</span>
+                    <p>L5 W5 D2</p>
                     <div className={`${styles.flex} ${styles.flex_gap}`}>
                         <Link href="#"><a>Play</a></Link>
                         <Link href="#"><a>Edit</a></Link>
                     </div>
                 </li>
                 <li className={`${styles.flex} ${styles.flex_between}`}>
-                    <span>L5 W5 D1</span>
+                    <p>L5 W5 D1</p>
                     <div className={`${styles.flex} ${styles.flex_gap}`}>
                         <Link href="#"><a>Play</a></Link>
                         <Link href="#"><a>Edit</a></Link>
                     </div>
                 </li>
                 <li className={`${styles.flex} ${styles.flex_between}`}>
-                    <span>L4 W5 D2</span>
+                    <p>L4 W5 D2</p>
                     <div className={`${styles.flex} ${styles.flex_gap}`}>
                         <Link href="#"><a>Play</a></Link>
                         <Link href="#"><a>Edit</a></Link>
                     </div>
                 </li>
                 <li className={`${styles.flex} ${styles.flex_between}`}>
-                    <span>L4 W5 D1</span>
+                    <p>L4 W5 D1</p>
                     <div className={`${styles.flex} ${styles.flex_gap}`}>
                         <Link href="#"><a>Play</a></Link>
                         <Link href="#"><a>Edit</a></Link>
                     </div>
                 </li>
                 <li className={`${styles.flex} ${styles.flex_between}`}>
-                    <span>L12 W5 D2</span>
+                    <p>L12 W5 D2</p>
                     <div className={`${styles.flex} ${styles.flex_gap}`}>
                         <Link href="#"><a>Play</a></Link>
                         <Link href="#"><a>Edit</a></Link>
                     </div>
                 </li>
                 <li className={`${styles.flex} ${styles.flex_between}`}>
-                    <span>L12 W5 D1</span>
+                    <p>L12 W5 D1</p>
                     <div className={`${styles.flex} ${styles.flex_gap}`}>
                         <Link href="#"><a>Play</a></Link>
                         <Link href="#"><a>Edit</a></Link>
