077d0666954d0e4bfa46cd2509e39b50ef788993
Mark O'Connor <mark.oconnor14@gmail.com>
2022-10-03 21:49:13 +0700

    set google fonts display to optional

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,9 +4,9 @@ export default function Document() {
   return (
     <Html>
       <Head>
-        <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Poppins"></link>
-        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@48,400,0,0" />
-        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200"
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Poppins&display=optional"></link>
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined&display=optional:opsz,wght,FILL,GRAD@48,400,0,0" />
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined&display=optional:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200"
         />
       </Head>
       <body>
